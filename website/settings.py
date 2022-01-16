417e43c4e93a1a63722169e85dbb2a461a15f0cf
krishivpiduri2010 <59217701+krishivpiduri2010@users.noreply.github.com>
2022-01-16 13:20:31 -0800

    message

--- website/settings.py
+++ website/settings.py
@@ -122,4 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT=BASE_DIR/'static'
+STATIC_ROOT=BASE_DIR/'staticfiles'
+STATICFILES_DIRS=[BASE_DIR/'staticfiles']
